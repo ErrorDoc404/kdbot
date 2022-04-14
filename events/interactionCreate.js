d6055fd6807e372c41efdeaceffa80dc59382620
ErrorDoc404 <akashsinhakvs@gmail.com>
2022-04-14 14:05:46 +0530

    Update interactionCreate.js

--- events/interactionCreate.js
+++ events/interactionCreate.js
@@ -1,3 +1,5 @@
+const { MessageEmbed } = require('discord.js');
+
 module.exports = async (client, interaction) => {
     let GuildDB = await client.GetGuild(interaction.guildId);
     if(!GuildDB) return console.log('Guild not Found');
@@ -12,11 +14,22 @@ module.exports = async (client, interaction) => {
     }
 
     const command = interaction.commandName;
-    
+
     let cmd = client.commands.get(command);
     if(!cmd) return;
 
     const args = interaction.options._hoistedOptions[0];
+
+    let embed = new MessageEmbed()
+      .setTitle("Premuim Required")
+      .setColor("RED")
+      .setDescription("Missing Permissions: You need the Special role to access this command. Ask Guild Admins for help")
+      .setFooter(
+        "If you think this as a bug, please report it in the support server!"
+      );
+
+    if(cmd.permissions.member == "ADMINISTRATOR" && !interaction.member.permissions.has('ADMINISTRATOR'))
+      return interaction.reply({embeds: [embed]}).catch((err) => {client.error(err)});
 
     if (cmd.SlashCommand && cmd.SlashCommand.run)
         cmd.SlashCommand.run(client, interaction, args, { GuildDB });
